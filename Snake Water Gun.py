5a2a39f02046af81601cdcac21cdb5bfdbce6a56
Nirajan Khatiwada <126379230+yenke-rajan@users.noreply.github.com>
2023-10-21 18:19:23 +0545

    Add files via upload

---
+++ Snake Water Gun.py
@@ -0,0 +1,52 @@
+import random
+
+def gameWin(comp, you):
+    # If two values are equal, declare a tie
+    if comp == you:
+        return None
+    
+    #Check for all possibilities when computer chose s
+    elif comp == 's':
+        if you == 'w':
+            return False
+        elif you=='g':
+            return True
+        
+    #Check for all possibilities when computer chose w
+    elif comp == 'w':
+        if you == 'g':
+            return False
+        elif you == 's':
+            return True
+    
+    #Check for all possibilities when computer chose g
+    elif comp == 'g':
+        if you == 's':
+            return False
+        elif you == 'w':
+            return True
+
+print("Computer Turn: Snake(s) Water (w) or Gun (g)?")
+
+randNo = random.randint(1,3)
+print(randNo)
+if randNo == 1:
+    comp='s'
+elif randNo == 2:
+    comp='w'
+elif randNo == 3:
+    comp='g'
+
+you = input("Your Turn: Snake(s) Water (w) or Gun (g)?")
+
+a = gameWin(comp, you)
+
+print(f"Computer Chose {comp}")
+print(f"You Chose {you}")
+
+if a == None:
+    print("The game is a tie!")
+elif a:
+    print("You Win!")
+else:
+    print("You Lose!")
